05906f8891ca25140479e4462f153c25f88436f0
SuraMahesh <surangamunasinghe22568@gmail.com>
2020-10-16 23:30:54 +0530

    update profile

--- riyapola/src/components/Dashboard/PublicProfile.js
+++ riyapola/src/components/Dashboard/PublicProfile.js
@@ -2,15 +2,15 @@ import React from 'react'
 import "./user.styles.scss";
 
 function PublicProfile() {
-    return (
-        <div className="container">
-        <div className="row" gutters-sm>
+  return (
+    <div className="container">
+      <div className="row" gutters-sm>
         <div className="col-md-4 d-none d-md-block">
           <div className="card">
             <div className="card-body">
               <nav className="nav flex-column nav-pills nav-gap-y-1">
                 <a
-                  href="#"
+                  href="creatorprofile"
                   data-toggle="tab"
                   className="nav-item nav-link has-icon nav-link-faded active"
                 >
@@ -28,7 +28,7 @@ function PublicProfile() {
                     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                     <circle cx="12" cy="7" r="4"></circle>
                   </svg>
-                  Profile Information
+                  My account
                 </a>
                 <a
                   href="accountsettings"
@@ -49,28 +49,9 @@ function PublicProfile() {
                     <circle cx="12" cy="12" r="3"></circle>
                     <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
                   </svg>
-                  Account Settings
+                  Privacy
                 </a>
-                <a
-                  href="security"
-                  data-toggle="tab"
-                  className="nav-item nav-link has-icon nav-link-faded"
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    className="feather feather-shield mr-2"
-                  >
-                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-                  </svg>
-                  Security
-                </a>
+
                 <a
                   href="notifications"
                   data-toggle="tab"
@@ -90,12 +71,15 @@ function PublicProfile() {
                     <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
                     <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
                   </svg>
-                  Notification
+                  Alerts
                 </a>
+
+
+
                 <a
                   href="billing"
                   data-toggle="tab"
-                  className="nav-item nav-link has-icon nav-link-faded"
+                  className="nav-item nav-link has-icon nav-link-faded active"
                 >
                   <svg
                     width="24"
@@ -118,8 +102,10 @@ function PublicProfile() {
                     ></rect>
                     <line x1="1" y1="10" x2="23" y2="10"></line>
                   </svg>
-                  Billing
+                  Listing Payment Status
                 </a>
+
+
               </nav>
             </div>
           </div>
@@ -198,7 +184,7 @@ function PublicProfile() {
                 </li>
                 <li className="nav-item">
                   <a
-                    href="#notification"
+                    href="#notifications"
                     data-toggle="tab"
                     className="nav-link has-icon"
                   >
@@ -251,24 +237,20 @@ function PublicProfile() {
             </div>
             <div className="card-body tab-content">
               <div className="tab-pane active" id="profile">
-                <h6>YOUR PROFILE INFORMATION</h6>
+                <h5>Update Account</h5>
                 <hr />
                 <form>
-                  <div className="form-group">
-                    <label htmlFor="fullName">User type</label>
-                    <p></p>
-                    <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false">
-                      Select user type
-                      </button>
-                    <div className="dropdown-menu dropdown-menu-lg-right">
-                      <button className="dropdown-item" type="button">Action</button>
-                      <button className="dropdown-item" type="button">Another action</button>
-                      <button className="dropdown-item" type="button">Something else here</button>
-                    </div>
+                <h6 className="topic">User Type</h6>
+                  <div className="form-group col-md-2">
+                    <select id="inputState" class="form-control">
+                      <option selected>User</option>
+                      <option>User </option>
+                      <option>Company</option>
+                    </select>
                   </div>
                   <p></p>
+                  <h6 className="topic">Full Name</h6>
                   <div className="form-group">
-                    <label htmlFor="fullName">Full Name</label>
                     <input
                       type="text"
                       className="form-control"
@@ -279,9 +261,10 @@ function PublicProfile() {
                     <small id="fullNameHelp" className="form-text text-muted">
                     </small>
                   </div>
+
                   <p></p>
+                  <h6 className="topic">Phone number</h6>
                   <div className="form-group">
-                    <label htmlFor="phone">Phone Number</label>
                     <input
                       type="number"
                       className="form-control"
@@ -290,8 +273,8 @@ function PublicProfile() {
                     ></input>
                   </div>
                   <p></p>
+                  <h6 className="topic">Cell phone number</h6>
                   <div className="form-group">
-                    <label htmlFor="phone">Cell Phone Number</label>
                     <input
                       type="number"
                       className="form-control"
@@ -300,47 +283,46 @@ function PublicProfile() {
                     ></input>
                   </div>
                   <p></p>
+                  <h6 className="topic">Country</h6>
                   <div className="form-group">
-                    <label htmlFor="fullName">Country</label>
                     <input
                       type="text"
                       className="form-control"
                       id="fullName"
                       aria-describedby="fullNameHelp"
-                      placeholder="Enter your country"
+                      placeholder="Srilanka"
                     ></input>
                     <small id="fullNameHelp" className="form-text text-muted">
                     </small>
                   </div>
                   <p></p>
-                  <div className="form-group">
-                    <label htmlFor="fullName">Region</label>
-                    <p></p>
-                    <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false">
-                      Select a region....
-                      </button>
-                    <div className="dropdown-menu dropdown-menu-lg-right">
-                      <button className="dropdown-item" type="button">Action</button>
-                      <button className="dropdown-item" type="button">Another action</button>
-                      <button className="dropdown-item" type="button">Something else here</button>
-                    </div>
+                  <h6 className="topic">Region</h6>
+                  <div className="form-group col-md-2">
+                    <select id="inputState" class="form-control">
+                      <option selected>Colombo</option>
+                      <option>Colombo </option>
+                      <option>Kandy</option>
+                      <option>Kurunegala</option>
+                      <option>Anuradhapuraya</option>
+                      <option>Jaffna</option>
+                      <option>Galle</option>
+                      <option>Matara</option>
+                    </select>
                   </div>
                   <p></p>
+                  <p></p>
+                  <h6 className="topic">City Area</h6>
                   <div className="form-group">
-                    <label htmlFor="fullName">City</label>
-                    <p></p>
-                    <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false">
-                      Select a City....
-                      </button>
-                    <div className="dropdown-menu dropdown-menu-lg-right">
-                      <button className="dropdown-item" type="button">Action</button>
-                      <button className="dropdown-item" type="button">Another action</button>
-                      <button className="dropdown-item" type="button">Something else here</button>
-                    </div>
+                    <input
+                      className="form-control autosize"
+                      id="bio"
+                      placeholder="Enter your City Area"
+                      style={{}}
+                    ></input>
                   </div>
                   <p></p>
+                  <h6 className="topic">Address</h6>
                   <div className="form-group">
-                    <label htmlFor="bio">Addres</label>
                     <input
                       className="form-control autosize"
                       id="bio"
@@ -349,8 +331,8 @@ function PublicProfile() {
                     ></input>
                   </div>
                   <p></p>
+                  <h6 className="topic">Website</h6>
                   <div className="form-group">
-                    <label htmlFor="url">Website</label>
                     <input
                       type="text"
                       className="form-control"
@@ -359,34 +341,35 @@ function PublicProfile() {
                     ></input>
                   </div>
                   <p></p>
+                  <h6 className="topic">Description</h6>
                   <div className="form-group">
-                    <label htmlFor="location">Description</label>
                     <textarea
                       className="form-control"
                       id="location"
-                      placeholder="Enter your location"
+                      placeholder="message"
                     ></textarea>
                   </div>
                   <div className="form-group small text-muted">
                   </div>
                   <p></p>
-                  <div className="details1">
                   <button type="button" className="btn btn-primary">
                     Update Profile
                   </button>
                   <button type="reset" className="btn btn-light">
                     Reset Changes
                   </button>
-                  </div>
+
+
                 </form>
+
               </div>
             </div>
           </div>
         </div>
       </div>
-            
-        </div>
-    )
+
+    </div>
+  )
 }
 
 export default PublicProfile
